423709321ff911f819d1f52c8dd64a38981eec52
Lauritz Holtmann <lauritz.holtmann@rub.de>
2018-03-27 20:08:12 +0200

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+
+<iframe width="100%" height="100%" src="test.php"></iframe>
+
+</body>
+</html>
---
+++ public/test.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *  Quick and dirty Implementation of Tracking POC, based on cached Redirects (Moved Permanently [301])
+ */
+    // Connect to db, we want to keep track of our visitors
+    $link = mysqli_connect('localhost', 'root', 'root', 'tracking');
+    if (!$link) die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+
+    // Determine if our User came from cached Redirect Page (we require that in this case the query Parameter 'id' is set
+    // Otherwise create new ID for User
+    if(isset($_REQUEST['id'])) {
+        echo "Your Tracking ID " . htmlentities($_REQUEST['id']);
+
+        // Update Timestamp in DB
+        $query = "UPDATE users SET last_seen = " . time() . " WHERE tracking_id = '" . mysqli_real_escape_string($link, $_REQUEST['id']) . "'";
+        mysqli_query($link, $query);
+    }
+    else {
+        // Create new ID, just for Demo purpose
+        $id = base64_encode(mt_rand(0, 999999999999));
+
+        echo "No Tracking ID given, create new ID: " . $id . "\n";
+
+        // Save new Tracking ID to DB
+        $query = "INSERT INTO `users` (`id`, `tracking_id`, `user_agent`, `last_seen`, `http_referer`) VALUES (NULL, '" . $id . "', '" . mysqli_real_escape_string($link, $_SERVER['HTTP_USER_AGENT']) . "', " . time() . ", '" . mysqli_real_escape_string($link, $_SERVER['HTTP_REFERER']) . "')";
+        mysqli_query($link, $query);
+
+        // Set HTTP Header
+        header('Location: test.php?id=' . $id, TRUE, 301);
+    }
+    mysqli_close($link);
